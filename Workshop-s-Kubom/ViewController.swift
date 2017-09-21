daffd68eaebb07ae16d4ad026bc46eb4d4452b7e
Michaela Janoskova <misela@Michaelas-MacBook-Pro.local>
2017-09-21 13:37:20 +0200

    zalozeni appky

--- Workshop-s-Kubom/ViewController.swift
+++ Workshop-s-Kubom/ViewController.swift
@@ -12,12 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   print ("AHOJKY, PRAJEM KRASNY DEN")
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction 
+    func mojeFunkce() {
+        
+        print("Jak se mas?")
+        
     }
 
 
